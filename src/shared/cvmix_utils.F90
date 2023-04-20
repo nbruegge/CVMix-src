98b234f82c59e7c90229fd1bcbc53f500d6df4b2
Nils Brüggemann <nils.brueggemann@mpimet.mpg.de>
2023-04-20 11:56:47 +0200

    Commented all tke calls out in src/shared/cvmix_utils.F90. Not sure why they were there, can probably be deleted.

--- src/shared/cvmix_utils.F90
+++ src/shared/cvmix_utils.F90
@@ -32,7 +32,7 @@ module cvmix_utils
 ! !PUBLIC MEMBER FUNCTIONS:
   public :: cvmix_update_wrap
   public :: cvmix_att_name
-  public :: cvmix_update_tke
+  !public :: cvmix_update_tke
   public :: solve_tridiag
 
 !EOP
@@ -212,8 +212,12 @@ contains
         cvmix_att_name = "Ssqr_iface"
       case("Nsqr_iface")
         cvmix_att_name = "Nsqr_iface"
-      case("TKE")
-        cvmix_att_name = "TKE"
+      case("tke")
+        cvmix_att_name = "tke"
+      case("tke_Lmix")
+        cvmix_att_name = "tke_Lmix"
+      case("tke_Pr")
+        cvmix_att_name = "tke_Pr"
       case("E_iw")
         cvmix_att_name = "E_iw"
       case("iw_diss")
@@ -269,87 +273,87 @@ contains
   end function cvmix_att_name
 
 
-!=================================================================================
-  subroutine cvmix_update_tke(old_vals,nlev, tke_diss_out,new_tke_diss, tke_out,new_tke, KappaM_out, new_KappaM,           &
-                              KappaH_out, new_KappaH,E_iw_out, new_E_iw,&
-                              iw_diss_out, new_iw_diss)
+!!=================================================================================
+!  subroutine cvmix_update_tke(old_vals,nlev, tke_diss_out,new_tke_diss, tke_out,new_tke, KappaM_out, new_KappaM,           &
+!                              KappaH_out, new_KappaH,E_iw_out, new_E_iw,&
+!                              iw_diss_out, new_iw_diss)
+!!
+!!! !DESCRIPTION:
+!!!  Update diffusivity values based on old_vals (either overwrite, sum, or find
+!!!  the level-by-level max)
+!!!  TKE & IDEMIX both use only the overwrite option
+!!
+!!! !INPUT PARAMETERS:
+!    integer, intent(in) :: old_vals, nlev
+!    real(cvmix_r8), dimension(nlev+1), optional, intent(in) ::     &
+!      new_KappaM                                                  ,& !
+!      new_KappaH                                                  ,& !
+!      new_E_iw                                                    ,& !
+!      new_iw_diss                                                 ,& !
+!      new_tke                                                     ,& !
+!      new_tke_diss                                                   !
 !
-!! !DESCRIPTION:
-!!  Update diffusivity values based on old_vals (either overwrite, sum, or find
-!!  the level-by-level max)
-!!  TKE & IDEMIX both use only the overwrite option
+!!! !OUTPUT PARAMETERS:
+!    real(cvmix_r8), dimension(nlev+1), optional, intent(inout) ::  & !
+!      KappaM_out                                                  ,& !
+!      KappaH_out                                                  ,& !
+!      E_iw_out                                                    ,& !
+!      iw_diss_out                                                 ,& !
+!      tke_out                                                     ,& !
+!      tke_diss_out                                                   !  
 !
-!! !INPUT PARAMETERS:
-    integer, intent(in) :: old_vals, nlev
-    real(cvmix_r8), dimension(nlev+1), optional, intent(in) ::     &
-      new_KappaM                                                  ,& !
-      new_KappaH                                                  ,& !
-      new_E_iw                                                    ,& !
-      new_iw_diss                                                 ,& !
-      new_tke                                                     ,& !
-      new_tke_diss                                                   !
-
-!! !OUTPUT PARAMETERS:
-    real(cvmix_r8), dimension(nlev+1), optional, intent(inout) ::  & !
-      KappaM_out                                                  ,& !
-      KappaH_out                                                  ,& !
-      E_iw_out                                                    ,& !
-      iw_diss_out                                                 ,& !
-      tke_out                                                     ,& !
-      tke_diss_out                                                   !  
-
-
-    integer :: kw
-
-
-    !CHARACTER(LEN=*), PARAMETER :: method_name = module_name//':cvmix_update_tke'
-
-
-
-    select case (old_vals)
-      case (CVMIX_SUM_OLD_AND_NEW_VALS)
-        if ((present(KappaM_out)).and.(present(new_KappaM))) &
-          KappaM_out = KappaM_out + new_KappaM
-        if ((present(KappaH_out)).and.(present(new_KappaH))) &
-          KappaH_out = KappaH_out + new_KappaH
-
-      case (CVMIX_MAX_OLD_AND_NEW_VALS)
-        do kw=1,nlev+1
-          if ((present(KappaM_out)).and.(present(new_KappaM))) &
-            KappaM_out(kw) = max(KappaM_out(kw), new_KappaM(kw))
-          if ((present(KappaH_out)).and.(present(new_KappaH))) &
-            KappaH_out(kw) = max(KappaH_out(kw), new_KappaH(kw))
-        end do
 !
-!      !This is the only option IDEMIX&TKE use to update to new values  
-      case (CVMIX_OVERWRITE_OLD_VAL)
-        if ((present(tke_diss_out)).and.(present(new_tke_diss))) then
-          tke_diss_out = new_tke_diss
-          print*, "updating tke_diss=",tke_diss_out
-        end if
-        if ((present(KappaH_out)).and.(present(new_KappaH))) &
-          KappaH_out = new_KappaH
-
-        if ((present(tke_out)).and.(present(new_tke))) then
-          tke_out = new_tke
-          print*, "updating tke"
-         end if
-        if ((present(KappaM_out)).and.(present(new_KappaM))) &
-          KappaM_out = new_KappaM
-
-        if ((present(E_iw_out)).and.(present(new_E_iw))) then
-          E_iw_out = new_E_iw
-        print*, "updating Internal wave energy"
-        end if
-        if ((present(iw_diss_out)).and.(present(new_iw_diss))) &
-          iw_diss_out = new_iw_diss
-      case DEFAULT
-        print*, "ERROR: do not know how to handle old values!"
-        stop 1
-        !CALL finish(method_name,'ERROR: do not know how to handle old values!')
-    end select
-
-  end subroutine cvmix_update_tke
+!    integer :: kw
+!
+!
+!    !CHARACTER(LEN=*), PARAMETER :: method_name = module_name//':cvmix_update_tke'
+!
+!
+!
+!    select case (old_vals)
+!      case (CVMIX_SUM_OLD_AND_NEW_VALS)
+!        if ((present(KappaM_out)).and.(present(new_KappaM))) &
+!          KappaM_out = KappaM_out + new_KappaM
+!        if ((present(KappaH_out)).and.(present(new_KappaH))) &
+!          KappaH_out = KappaH_out + new_KappaH
+!
+!      case (CVMIX_MAX_OLD_AND_NEW_VALS)
+!        do kw=1,nlev+1
+!          if ((present(KappaM_out)).and.(present(new_KappaM))) &
+!            KappaM_out(kw) = max(KappaM_out(kw), new_KappaM(kw))
+!          if ((present(KappaH_out)).and.(present(new_KappaH))) &
+!            KappaH_out(kw) = max(KappaH_out(kw), new_KappaH(kw))
+!        end do
+!!
+!!      !This is the only option IDEMIX&TKE use to update to new values  
+!      case (CVMIX_OVERWRITE_OLD_VAL)
+!        if ((present(tke_diss_out)).and.(present(new_tke_diss))) then
+!          tke_diss_out = new_tke_diss
+!          print*, "updating tke_diss=",tke_diss_out
+!        end if
+!        if ((present(KappaH_out)).and.(present(new_KappaH))) &
+!          KappaH_out = new_KappaH
+!
+!        if ((present(tke_out)).and.(present(new_tke))) then
+!          tke_out = new_tke
+!          print*, "updating tke"
+!         end if
+!        if ((present(KappaM_out)).and.(present(new_KappaM))) &
+!          KappaM_out = new_KappaM
+!
+!        if ((present(E_iw_out)).and.(present(new_E_iw))) then
+!          E_iw_out = new_E_iw
+!        print*, "updating Internal wave energy"
+!        end if
+!        if ((present(iw_diss_out)).and.(present(new_iw_diss))) &
+!          iw_diss_out = new_iw_diss
+!      case DEFAULT
+!        print*, "ERROR: do not know how to handle old values!"
+!        stop 1
+!        !CALL finish(method_name,'ERROR: do not know how to handle old values!')
+!    end select
+!
+!  end subroutine cvmix_update_tke
 
 
 !=================================================================================
