5cad521509d7d701a7014f73b5a4cbb1ebabcb64
Nils Brüggemann <nils.brueggemann@mpimet.mpg.de>
2023-04-20 12:03:57 +0200

    Added configuration for testing tke in reg_tests/tke/.

---
+++ reg_tests/tke/input.nl
@@ -0,0 +1,5 @@
+&cvmix_nml
+mix_type = 'tke'
+nlev     = 10
+max_nlev = 10
+/
---
+++ reg_tests/tke/tke-test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+path_output='out/'
+
+#cat > ${path_output}/input.nl << EOF
+cat > input.nl << EOF
+&cvmix_nml
+mix_type = 'tke'
+nlev     = 10
+max_nlev = 10
+/
+EOF
+
+# (1) Load required routines
+. ../common/environ.sh
+. ../common/usage.sh
+. ../common/parse_inputs.sh
+. ../common/build.sh
+. ../common/run.sh
+
+rm -rf path_output
+mkdir path_output
+
+parse_inputs $@
+build
+run
+
+## (2) Look at output
+#if [ "${USE_NETCDF}" == "netcdf" ]; then
+#  ncdump -v Tdiff data_LMD.nc
+#  ncdump -v Mdiff data_PP1d.nc
+#else
+#  cat data_LMD.out
+#  cat data_PP1d.out
+#fi
